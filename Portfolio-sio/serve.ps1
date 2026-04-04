dff375d7f55b6cb69ba73e71f567041ba4ef616a
Louis3701 <louis.caron16@gmail.com>
2026-04-04 18:36:11 +0200

    tout es bon

---
+++ Portfolio-sio/serve.ps1
@@ -0,0 +1,42 @@
+$path = "docs"
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:8000/")
+$listener.Start()
+Write-Host "Serving portfolio at http://localhost:8000"
+while ($listener.IsListening) {
+    $context = $listener.GetContext()
+    $request = $context.Request
+    $response = $context.Response
+    $localPath = $request.Url.LocalPath
+    if ($localPath -eq "/") { $localPath = "/index.html" }
+    $filePath = Join-Path $path $localPath.TrimStart("/")
+    if (Test-Path $filePath -PathType Leaf) {
+        $content = Get-Content $filePath -Raw -Encoding UTF8
+        $ext = [System.IO.Path]::GetExtension($filePath)
+        switch ($ext) {
+            ".html" { $response.ContentType = "text/html; charset=utf-8" }
+            ".css" { $response.ContentType = "text/css; charset=utf-8" }
+            ".js" { $response.ContentType = "application/javascript; charset=utf-8" }
+            ".png" { $response.ContentType = "image/png" }
+            ".jpg" { $response.ContentType = "image/jpeg" }
+            ".jpeg" { $response.ContentType = "image/jpeg" }
+            ".gif" { $response.ContentType = "image/gif" }
+            ".svg" { $response.ContentType = "image/svg+xml" }
+            ".ico" { $response.ContentType = "image/x-icon" }
+            ".woff" { $response.ContentType = "font/woff" }
+            ".woff2" { $response.ContentType = "font/woff2" }
+            default { $response.ContentType = "text/plain; charset=utf-8" }
+        }
+        $response.StatusCode = 200
+        $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
+        $response.ContentLength64 = $buffer.Length
+        $response.OutputStream.Write($buffer, 0, $buffer.Length)
+    } else {
+        $response.StatusCode = 404
+        $notFound = "<h1>404 Not Found</h1>"
+        $buffer = [System.Text.Encoding]::UTF8.GetBytes($notFound)
+        $response.ContentLength64 = $buffer.Length
+        $response.OutputStream.Write($buffer, 0, $buffer.Length)
+    }
+    $response.OutputStream.Close()
+}
