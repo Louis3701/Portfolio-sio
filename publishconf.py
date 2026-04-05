9c100f3309cbd1704d7f7201ef11aac93b7da49c
Louis3701 <louis.caron16@gmail.com>
2026-04-05 22:58:41 +0200

    Fix GitHub Pages base URL for repository pages and regenerate site

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,6 @@ from pelicanconf import *
 
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://louis3701.github.io"
+SITEURL = "https://louis3701.github.io/Portfolio-sio"
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
